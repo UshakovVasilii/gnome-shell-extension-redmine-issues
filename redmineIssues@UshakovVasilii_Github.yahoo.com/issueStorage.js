65da282ef8c4975154e50ab112ff5fea41b11999
UshakovVasilii <UshakovVasilii@gmail.com>
2014-03-06 00:09:49 +0400

    Store issues in file

--- redmineIssues@UshakovVasilii_Github.yahoo.com/issueStorage.js
+++ redmineIssues@UshakovVasilii_Github.yahoo.com/issueStorage.js
@@ -1,8 +1,11 @@
 const Lang = imports.lang;
+const Shell = imports.gi.Shell;
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
 
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const Convenience = Me.imports.convenience;
+//const ExtensionUtils = imports.misc.extensionUtils;
+//const Me = ExtensionUtils.getCurrentExtension();
+//const Convenience = Me.imports.convenience;
 
 const LABEL_KEYS = [
     'status',
@@ -19,13 +22,38 @@ const IssueStorage = new Lang.Class({
     Name: 'IssueStorage',
 
     _init: function() {
-        this._settings = Convenience.getSettings();
+        //this._settings = Convenience.getSettings();
         this.issues = {};
 
-        this._settings.get_strv('issues').forEach(Lang.bind(this, function(s){
+        let path = GLib.build_pathv('/', [GLib.get_user_data_dir(), 'redmine-issues']);
+        if(!GLib.file_test(path, GLib.FileTest.EXISTS))
+            GLib.mkdir_with_parents(path, 493);
+
+        let issuesPath = GLib.build_filenamev([path, 'issues.json']);
+        if(!GLib.file_test(issuesPath, GLib.FileTest.EXISTS))
+            GLib.file_set_contents(issuesPath, '');
+
+        this._loadLines().forEach(Lang.bind(this, function(s){
+            if(!s)
+                return;
             let issue = JSON.parse(s);
             this.issues[issue.id] = issue;
         }));
+    },
+
+    _loadLines : function(){
+        //return this._settings.get_strv('issues');
+        var issuesFile = Gio.file_new_for_path(GLib.get_user_data_dir() + '/redmine-issues/issues.json');
+        return Shell.get_file_contents_utf8_sync(issuesFile.get_path()).split('\n');
+    },
+
+    _saveLines : function(lines){
+        //this._settings.set_strv('issues', lines);
+        let file = Gio.file_new_for_path(GLib.get_user_data_dir() + '/redmine-issues/issues.json');
+        let out = file.replace(null, false, Gio.FileCreateFlags.NONE, null);
+        Shell.write_string_to_stream(out, lines.join('\n'));
+        out.close(null);
+        //global.log('[RI] saved');
     },
 
     updateIssue : function(issue){
@@ -37,7 +65,7 @@ const IssueStorage = new Lang.Class({
         for(let i in this.issues){
             data.push(JSON.stringify(this.issues[i]));
         }
-        this._settings.set_strv('issues', data);
+        this._saveLines(data);
         this.issues[issue.id] = issue;
         return true;
     },
@@ -58,9 +86,9 @@ const IssueStorage = new Lang.Class({
             if(value || value==0)
                 issue.unread_fields.push(jsonKey);
         });
-        let data = this._settings.get_strv('issues');
+        let data = this._loadLines();
         data.push(JSON.stringify(issue));
-        this._settings.set_strv('issues', data);
+        this._saveLines(data);
         this.issues[issue.id] = issue;
         return true;
     },
@@ -71,7 +99,7 @@ const IssueStorage = new Lang.Class({
         for(let i in this.issues){
             data.push(JSON.stringify(this.issues[i]));
         }
-        this._settings.set_strv('issues', data);
+        this._saveLines(data);
         return true;
     },
 
